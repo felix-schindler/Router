ea2649837966b1f58f9e7602393c7afd62079318
Felix <felix-schindler@outlook.com>
2021-12-02 16:18:07 +0100

    Fix frontend controller; Update workflow

--- Backend/Controllers/FrontendController.php
+++ Backend/Controllers/FrontendController.php
@@ -2,16 +2,6 @@
 
 class FrontendController extends Controller
 {
-	protected function getRoutes() : array
-	{
-		return [];
-	}
-
-	protected function userRequired() : bool
-	{
-		return false;
-	}
-
 	public function execute() : void
 	{
 		(new FrontendView())->render();
